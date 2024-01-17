36b4d07f285c8f53af8e88667163a3b885a534a0
matthew rendely <matthew.rendely@gmail.com>
2024-01-16 23:51:39 -0800

    Add basic accelerate and breaking

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const universe = document.getElementById('universe');
 let pause = false;
-document.addEventListener('keydown', (e) => pause = true);
+let accelerate = false;
+let breaking = false; 
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') pause = true
+    if (e.code === 'KeyA') accelerate = true
+    if (e.code === 'KeyB') breaking = true
+});
 document.addEventListener('keyup', (e) => {
-    pause = false;
-    main()
+    if (e.code === 'Space') {
+        pause = false;
+        main()
+    }
+    if (e.code === 'KeyA') accelerate = false
+    if (e.code === 'KeyB') breaking = false
 });
 
 
@@ -22,8 +32,8 @@ class Body {
 }
 
 const bodies = [
-    new Body('small', 'red', 5, 10, 150, 250, 0, 12),
-    new Body('small2', 'blue', 5, 10, 450, 250, 0, -12),
+    new Body('small1', 'red', 5, 10, 150, 250, 0, 12),
+    // new Body('small2', 'blue', 5, 10, 450, 250, 0, -12),
     // new Body('small3', 'orange',  5, 10, 100, 250, 0, 12),
     new Body('big', 'white', 15, 3000, 250, 250)
 ]
@@ -52,26 +62,36 @@ bodyInteraction = (a, b) => {
     const theta = Math.atan2(yDist, xDist);
     const xAcc = 1/r*Math.cos(theta)*b.m/a.m;
     const yAcc = -1/r*Math.sin(theta)*b.m/a.m;
+    
     a.vx += xAcc;
     a.vy += yAcc;
+
+    const velTheta = Math.atan2(-a.vy, a.vx);
+
+    if (a.id === 'small1' && (accelerate || breaking)){
+        console.log('accelerating!');
+        const dir = accelerate ? 1 : -1;
+        a.vx += dir * Math.cos(velTheta);
+        a.vy += -dir * Math.sin(velTheta);
+    }
+
+    // if (a.id === 'small1') console.log({
+    //     vx: a.vx,
+    //     vy: -a.vy,
+    //     angle: velTheta * 180 / Math.PI
+    // });
+
+    // Stop if they impact, need ot clean up 
     // if (r <= a.r + b.r+15) {
     //     a.vx = 0;
     //     a.vy = 0;
     // }
+
     a.x += a.vx;
     a.y += a.vy;
     a.el.style.top = a.y - a.r/2 + 'px';
     a.el.style.left = a.x - a.r/2 + 'px';
-    // console.table({
-    //     deg: theta*180/Math.PI, 
-    //     sin: Math.sin(theta),
-    //     cos: Math.cos(theta),
-    //     xDist, 
-    //     yDist, 
-    //     theta, 
-    //     xAcc, 
-    //     yAcc
-    // })
+    
 }
 
 const maxCycles = 10000/20;
