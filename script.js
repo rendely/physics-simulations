d8375a1a6d12daaa3404eed3a1002e4978094d95
matthew rendely <matthew.rendely@gmail.com>
2024-01-16 21:16:05 -0800

    Prototyping gravity, stuck debugging theta calc

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const universe = document.getElementById('universe');
 
 class Body {
-    constructor(id, x, y, vx=0, vy=0) {
+    constructor(id, r, x, y, vx=0, vy=0) {
       this.id = id;
+      this.r = r;
       this.x = x;
       this.y = y;
       this.vx = vx;
@@ -12,8 +13,8 @@ class Body {
 }
 
 const bodies = [
-    new Body('big', 400, 250, -10, -15 ),
-    new Body('small', 100, 250, 10, -20)    
+    new Body('small', 10, 100, 270, 0, -10),
+    new Body('big', 30, 250, 250)
 ]
 
 bodies.forEach(b => {
@@ -22,21 +23,35 @@ bodies.forEach(b => {
     newDiv.id = b.id;
     newDiv.style.top = b.y + 'px';
     newDiv.style.left = b.x + 'px';
+    newDiv.style.width = b.r + 'px';
+    newDiv.style.height = b.r + 'px';
     b.el = newDiv;
     universe.appendChild(newDiv);
 })
 
-const maxCycles = 2000/20;
+
+bodyInteraction = (a, b) => {
+    const newNode = a.el.cloneNode();
+    universe.append(newNode);
+    const xDist = a.x - b.x;
+    const yDist = a.y - b.y;
+    const r = Math.sqrt( xDist**2 + yDist**2);
+    const theta = Math.atan(yDist / xDist);
+    const xAcc = 300/r*Math.cos(theta);
+    const yAcc = 300/r*Math.sin(theta);
+    a.vx += xAcc;
+    a.vy += yAcc;
+    a.x += a.vx;
+    a.y += a.vy;
+    a.el.style.top = a.y + 'px';
+    a.el.style.left = a.x + 'px';
+    console.table({xDist, yDist, theta, deg: theta*180/Math.PI, xAcc, yAcc})
+}
+
+const maxCycles = 300/20;
 let i = 0;
 main = () => {
-    bodies.forEach(b => {
-        console.log(b);
-        b.vy += 1;
-        b.y += b.vy;
-        b.x += b.vx;
-        b.el.style.top = b.y + 'px';
-        b.el.style.left = b.x + 'px';
-    })
+    bodyInteraction(bodies[0], bodies[1]);
     i++
     if (i < maxCycles) setTimeout(main, 20)
 }
